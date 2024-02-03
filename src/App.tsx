85110740a8b4cb707e6055f1a0192dcd9149df9d
Ivan Escobar <ivnescbr1989@gmail.com>
2024-02-02 21:12:21 -0500

    styling: main content

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <div className="text-right p-5 text-3xl border-b shadow-md shadow-black">
         <section
           id="my-name"
-          className="border p-3 inline-block rounded-md shadow-md shadow-black font-Honk bg-blue-900 hover:bg-white"
+          className="border p-3 inline-block rounded-md shadow-md shadow-black font-Honk bg-blue-900 hover:bg-slate-400"
         >
           Ivan Felipe Escobar
         </section>
@@ -18,7 +18,7 @@ function App() {
         <Nav />
       </nav>
     </header>
-    <body id="">
+    <body id="main_content" className="border mx-5 h-96 shadow-black rounded-md shadow-md bg-stone-800">
 
     </body>
     <footer id="footer" className="p-3 absolute bottom-0 border-t w-full shadow-reverse shadow-black">
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,7 +5,7 @@ const Nav = () => {
   return (
     <ul id="navigation-links" className="flex justify-end gap-3">
        {
-links.map( link => <li key={link} className="p-2 border bg-blue-900 shadow-md shadow-black text-right rounded-md font-Honk text-xl hover:bg-white">{link}</li>)
+links.map( link => <li key={link} className="p-2 border bg-blue-900 shadow-md shadow-black text-right rounded-md font-Honk text-xl hover:bg-slate-600">{link}</li>)
        }
     </ul>
   )
